920a7a33ea064a88f9ab7651510d90b2eeee6572
Shreyansh Lodha <me@staticvar.dev>
2026-07-13 20:38:07 +0530

    fix: avoid tracking permission repair on symlinks

--- internal/tracking/permissions_unix.go
+++ internal/tracking/permissions_unix.go
@@ -2,24 +2,36 @@
 
 package tracking
 
-import "os"
-
-const (
-	privateTrackingDirMode  = 0o700
-	privateTrackingFileMode = 0o600
+import (
+	"fmt"
+	"os"
 )
 
+const privateTrackingDirMode = 0o700
+
 func ensureTrackingDir(dir string) error {
+	info, err := os.Lstat(dir)
+	if err == nil {
+		if info.Mode()&os.ModeSymlink != 0 {
+			return fmt.Errorf("tracking directory is a symlink: %s", dir)
+		}
+		if !info.IsDir() {
+			return fmt.Errorf("tracking directory is not a directory: %s", dir)
+		}
+		return nil
+	}
+	if !os.IsNotExist(err) {
+		return err
+	}
 	if err := os.MkdirAll(dir, privateTrackingDirMode); err != nil {
 		return err
 	}
-	return os.Chmod(dir, privateTrackingDirMode)
-}
-
-func secureTrackingFile(file *os.File) error {
-	return file.Chmod(privateTrackingFileMode)
-}
-
-func secureTrackingPath(path string) error {
-	return os.Chmod(path, privateTrackingFileMode)
+	info, err = os.Lstat(dir)
+	if err != nil {
+		return err
+	}
+	if info.Mode()&os.ModeSymlink != 0 || !info.IsDir() {
+		return fmt.Errorf("tracking directory changed while creating it: %s", dir)
+	}
+	return nil
 }
--- internal/tracking/permissions_windows.go
+++ internal/tracking/permissions_windows.go
@@ -2,16 +2,26 @@
 
 package tracking
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 func ensureTrackingDir(dir string) error {
-	return os.MkdirAll(dir, 0o755)
-}
-
-func secureTrackingFile(*os.File) error {
-	return nil
-}
-
-func secureTrackingPath(string) error {
-	return nil
+	info, err := os.Lstat(dir)
+	if err == nil {
+		if info.Mode()&os.ModeSymlink != 0 {
+			return fmt.Errorf("tracking directory is a symlink: %s", dir)
+		}
+		if !info.IsDir() {
+			return fmt.Errorf("tracking directory is not a directory: %s", dir)
+		}
+		return nil
+	}
+	if !os.IsNotExist(err) {
+		return err
+	}
+	// Windows access is controlled by the user's profile ACLs; Go file modes are
+	// mode-neutral there, so do not repair modes on existing path targets.
+	return os.MkdirAll(dir, 0o700)
 }
